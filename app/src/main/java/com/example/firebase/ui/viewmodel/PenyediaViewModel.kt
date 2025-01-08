2900b5c3fb15d25c09014230fb173552e29c55c1
alviennn <alvien.rnp@gmail.com>
2025-01-08 15:04:20 +0700

    commit kelas

---
+++ app/src/main/java/com/example/firebase/ui/viewmodel/PenyediaViewModel.kt
@@ -0,0 +1,16 @@
+package com.example.firebase.ui.viewmodel
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.firebase.MahasiswaApplications
+
+object PenyediaViewModel{
+    val Factory = viewModelFactory {
+        initializer { HomeViewModel(AplikasiMahasiswa().container.mahasiswaRepository) }
+    }
+}
+fun CreationExtras.AplikasiMahasiswa(): MahasiswaApplications =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as MahasiswaApplications)
