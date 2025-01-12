86c8475ea4d9ad332ece23bdf4b590ab9993d388
alviennn <alvien.rnp@gmail.com>
2025-01-12 10:47:18 +0700

    Menambahkan fungsi untuk mengambil daftar data mahasiswa dari Firestore menggunakan Flow

--- app/src/main/java/com/example/firebase/repository/MahasiswaRepository.kt
+++ app/src/main/java/com/example/firebase/repository/MahasiswaRepository.kt
@@ -2,7 +2,10 @@ package com.example.firebase.repository
 
 import com.example.firebase.model.Mahasiswa
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
+import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.tasks.await
 
 interface MahasiswaRepository{
@@ -25,6 +28,22 @@ class NetworkRepositoryMhs(
             firestore.collection("Mahasiswa").add(mahasiswa).await()
         } catch (e: Exception) {
             throw Exception("Gagal menambahkan data Mahasiswa: ${e.message}")
+        }
+    }
+
+    override suspend fun getMahasiswa(): Flow<List<Mahasiswa>> = callbackFlow {
+        val mhsCollection = firestore.collection("Mahasiswa")
+            .orderBy("nim", Query.Direction.DESCENDING)
+            .addSnapshotListener { value, error ->
+                if (value != null) {
+                    val mhsList = value.documents.mapNotNull {
+                        it.toObject(Mahasiswa::class.java)!!
+                    }
+                    trySend(mhsList) // try send memberikan fungsi untuk mengirim data ke flow
+                }
+            }
+        awaitClose {
+            mhsCollection.remove()
         }
     }
 }
