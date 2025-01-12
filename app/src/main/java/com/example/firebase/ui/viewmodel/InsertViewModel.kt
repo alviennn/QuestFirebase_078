9ddd3ee9127c9332ba54c08ce78ea4c71fd5d019
alviennn <alvien.rnp@gmail.com>
2025-01-12 11:33:16 +0700

    Menambahkan data class FormErrorState untuk menangani validasi dan pesan kesalahan pada form mahasiswa

--- app/src/main/java/com/example/firebase/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/firebase/ui/viewmodel/InsertViewModel.kt
@@ -2,6 +2,26 @@ package com.example.firebase.ui.viewmodel
 
 import com.example.firebase.model.Mahasiswa
 
+data class FormErrorState(
+    val nim: String? = null,
+    val nama: String? = null,
+    val jenis_Kelamin: String? = null,
+    val alamat: String? = null,
+    val kelas: String? = null,
+    val angkatan: String? = null
+)
+
+//data class Variabel yang menyimpan data input form
+data class MahasiswaEvent(
+    val nim: String = "",
+    val nama: String = "",
+    val jenis_Kelamin: String = "",
+    val alamat: String = "",
+    val kelas: String = "",
+    val angkatan: String = ""
+)
+
+//Menyimpan input form ke dalam entity
 fun MahasiswaEvent.toMhsModel() : Mahasiswa = Mahasiswa (
     nim = nim,
     nama = nama,
