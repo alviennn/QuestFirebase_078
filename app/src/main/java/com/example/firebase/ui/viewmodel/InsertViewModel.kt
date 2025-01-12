c670bd5b8ec9d4d0e12f8508067ba550debbb5e5
alviennn <alvien.rnp@gmail.com>
2025-01-12 11:42:09 +0700

    Menambahkan class FormState untuk merepresentasikan status form dan data class InsertUiState untuk mengelola UI state pada proses input mahasiswa. dan memperbaiki error pada MahasiswaEvent

--- app/src/main/java/com/example/firebase/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/firebase/ui/viewmodel/InsertViewModel.kt
@@ -2,6 +2,18 @@ package com.example.firebase.ui.viewmodel
 
 import com.example.firebase.model.Mahasiswa
 
+sealed class FormState{
+    object Idle : FormState()
+    object Loading : FormState()
+    data class Success(val message: String) : FormState()
+    data class Error(val message: String) : FormState()
+}
+
+data class InsertUiState(
+    val insertUiEvent: MahasiswaEvent = MahasiswaEvent(),
+    val isEntryValid: FormErrorState = FormErrorState(),
+)
+
 data class FormErrorState(
     val nim: String? = null,
     val nama: String? = null,
@@ -9,7 +21,12 @@ data class FormErrorState(
     val alamat: String? = null,
     val kelas: String? = null,
     val angkatan: String? = null
-)
+){
+    fun isValid(): Boolean {
+        return nim != null && nama != null && jenis_Kelamin != null &&
+                alamat != null && kelas != null && angkatan != null
+    }
+}
 
 //data class Variabel yang menyimpan data input form
 data class MahasiswaEvent(
@@ -25,7 +42,7 @@ data class MahasiswaEvent(
 fun MahasiswaEvent.toMhsModel() : Mahasiswa = Mahasiswa (
     nim = nim,
     nama = nama,
-    jenis_Kelamin = jenis_Kelamin,
+    jenis_kelamin = jenis_Kelamin,
     alamat = alamat,
     kelas = kelas,
     angkatan = angkatan
