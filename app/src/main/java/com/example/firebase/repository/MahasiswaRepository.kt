014906b07cc44ede9121df2d590bbd8aa279ffec
alviennn <alvien.rnp@gmail.com>
2025-01-12 10:42:55 +0700

    Menambahkan fungsi untuk menyimpan data mahasiswa ke Firestore

--- app/src/main/java/com/example/firebase/repository/MahasiswaRepository.kt
+++ app/src/main/java/com/example/firebase/repository/MahasiswaRepository.kt
@@ -1,7 +1,9 @@
 package com.example.firebase.repository
 
 import com.example.firebase.model.Mahasiswa
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.tasks.await
 
 interface MahasiswaRepository{
     suspend fun getMahasiswa(): Flow<List<Mahasiswa>>
@@ -13,5 +15,17 @@ interface MahasiswaRepository{
     suspend fun deleteMahasiswa(nim: String)
 
     suspend fun getMahasiswaById(nim: String): Flow<Mahasiswa?>
+}
+
+class NetworkRepositoryMhs(
+    private val firestore: FirebaseFirestore
+) : MahasiswaRepository {
+    override suspend fun insertMahasiswa(mahasiswa: Mahasiswa) {
+        try {
+            firestore.collection("Mahasiswa").add(mahasiswa).await()
+        } catch (e: Exception) {
+            throw Exception("Gagal menambahkan data Mahasiswa: ${e.message}")
+        }
+    }
 }
 
